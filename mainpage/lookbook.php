6faf48640270235a44c39deadeb7bbe77d98d15b
Orane Mendes <oranemendes@MacBook-Pro-de-Orane.local>
2018-11-07 20:44:39 +0100

    Début de la mise en page des cards

--- mainpage/lookbook.php
+++ mainpage/lookbook.php
@@ -4,6 +4,8 @@
 <html>
 <head>
     <meta charset="utf8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link rel="stylesheet" type="text/css" href="../css/stylenav.css" />
     <title>Orane MUA - Look Book</title>
@@ -15,32 +17,31 @@
 require_once '../database.php';
 ?>
 
+<header class="header"><?php include('../includes/header.php') ?></header>
+
 <div class="page">
-    <header class="header"><?php include('../includes/header.php') ?></header>
+
     <nav><?php include('../includes/nav.php') ?> </nav>
 
     <?php
-    $req = $db->query('SELECT * FROM article WHERE category = "lookbook"');
+    $req = $db->query('SELECT * FROM article WHERE category = "lookbook" ORDER BY id DESC');
     $articles = $req->fetchAll();
 
-    foreach ($articles as $article): ?>
+     foreach ($articles as $article): ?>
 
-        <div class="row">
-            <div class="col s12 m7">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="images/sample-1.jpg">
-                        <span class="card-title"><?= $article['name'] ?></span>
-                    </div>
-                    <div class="card-content">
-                        <p><?= $article['content'] ?></p>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">This is a link</a>
-                    </div>
+        <div class="container">
+            <div class="card" class="col-md-4 col-xs-12 col-sm-4 col-lg-4">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title"><?= $article['name'] ?></h3>
+                    <a href="#" class="btn btn-primary">Lire l'article</a>
+                    <p class="card-text"><?= $article['content'] ?></p>
                 </div>
             </div>
         </div>
+
+
+
     <?php endforeach ?>
 
 
